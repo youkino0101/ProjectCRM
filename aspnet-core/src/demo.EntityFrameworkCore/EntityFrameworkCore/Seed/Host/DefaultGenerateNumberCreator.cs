d71022c672982cc5c3fafdb75b876773b6c25fd7
Nguyễn Thanh Quan <quan.nguyenthanh@dtp000.top>
2023-08-24 17:30:28 +0700

    feat: add init generate

---
+++ aspnet-core/src/demo.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/DefaultGenerateNumberCreator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Abp.Localization;
+using Abp.MultiTenancy;
+using demo.Extensions;
+
+namespace demo.EntityFrameworkCore.Seed.Host
+{
+    public class DefaultGenerateNumberCreator
+    {
+        public static List<GenerateNumber> InitialGenerateNumber => GetGenerateNumber();
+
+        private static List<GenerateNumber> GetGenerateNumber()
+        {
+            return new List<GenerateNumber>
+            {
+                new GenerateNumber(){Code ="P", Description ="Product", CurrentNumber = 0, Digit = 6},
+            };
+        }
+
+        private readonly demoDbContext _context;
+
+        public DefaultGenerateNumberCreator(demoDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Create()
+        {
+            CreateGenerateNumber();
+        }
+
+        private void CreateGenerateNumber()
+        {
+            foreach (var data in InitialGenerateNumber)
+            {
+                AddGenerateNumberIfNotExists(data);
+            }
+        }
+
+        private void AddGenerateNumberIfNotExists(GenerateNumber data)
+        {
+            if (_context.GenerateNumber.IgnoreQueryFilters().Any(l => l.Code == data.Code && l.Description == data.Description))
+            {
+                return;
+            }
+
+            _context.GenerateNumber.Add(data);
+            _context.SaveChanges();
+        }
+    }
+}
